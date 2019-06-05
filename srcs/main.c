50f6b5515dfe5352ba7a363219960280c177a507
Emerita Medea <emedea@oa-l6.21-school.ru>
2019-06-05 15:44:36 +0300

    final

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: fmelda <fmelda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 12:49:39 by emedea            #+#    #+#             */
-/*   Updated: 2019/06/05 13:59:40 by emedea           ###   ########.fr       */
+/*   Updated: 2019/06/05 15:41:57 by emedea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,40 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+static char			*read_file(int argc, char *argv)
+{
+	int				amount;
+	char			buffer[547];
+	char			*output;
+
+	if (argc != 2)
+	{
+		write(1, "error\n", 6);
+		return (NULL);
+	}
+	amount = read(open(argv, O_RDONLY), buffer, 546);
+	if (amount < 0)
+	{
+		write(1, "error\n", 6);
+		return (NULL);
+	}
+	buffer[amount] = '\0';
+	output = ft_strdup(buffer);
+	return (output);
+}
+
 int					main(int argc, char **argv)
 {
-	char			buffer[547];
 	char			*input;
 	int				amount;
 	t_tetriminos	*objects;
 	t_map			map;
 
-	buffer[read(open(argv[1], O_RDONLY), buffer, 546)] = '\0';
-	input = ft_strdup(buffer);
+	input = read_file(argc, argv[1]);
+	if (!input)
+		return (0);
 	amount = amount_tetriminos(input);
-	if (validation(input, amount) || argc != 2)
+	if (validation(input, amount))
 	{
 		objects = fill_objects(input, amount);
 		if (!objects)
