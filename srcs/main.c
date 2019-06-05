1b2aa77bbae68090e79af50bfad11e2eb6c6feb2
Felton Melda <fmelda@oa-l5.21-school.ru>
2019-06-05 13:56:38 +0300

    gg

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emedea <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: fmelda <fmelda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 12:49:39 by emedea            #+#    #+#             */
-/*   Updated: 2019/06/05 13:31:06 by emedea           ###   ########.fr       */
+/*   Updated: 2019/06/05 13:53:43 by fmelda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ int					main(int argc, char **argv)
 	{
 		objects = fill_objects(input, amount);
 		map.amount = amount;
-        map.size = minimal_map_size(amount);
+		map.size = minimal_map_size(amount);
 		get_solution(objects, &map);
 		write(1, map.solution, (map.width - 1));
-        free(objects);
+		free(objects);
 	}
 	else
 		write(1, "error\n", 6);
-    free(input);
+	free(input);
 	return (0);
 }
